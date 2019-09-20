21ffaef4e4ed3f6bef10bc0806cb78b5a217e2e4
JoseBarbero <jabarbero@ubu.es>
2019-09-20 13:08:56 +0200

    Target column selection

--- ubumlaas/experiments/forms.py
+++ ubumlaas/experiments/forms.py
@@ -7,6 +7,7 @@ from flask_wtf.file import FileField, FileAllowed
 from flask import flash
 from flask_login import current_user
 from ubumlaas.models import User, get_algorithms
+import pandas as pd
 import os
 
 
@@ -19,7 +20,7 @@ class ExperimentForm(FlaskForm):
                            choices=[])
 
     data = SelectField("Dataset", validators=[DataRequired()],
-                       choices=[])
+                       choices=[("","---")])
 
     def alg_list(self, alg_typ):
         self.alg_name.choices = [(x.alg_name, x.web_name)
@@ -27,7 +28,7 @@ class ExperimentForm(FlaskForm):
 
     def dataset_list(self):
         if os.path.isdir("ubumlaas/datasets/"+current_user.username):
-            self.data.choices = [(x, x) for x in os.listdir(
+            self.data.choices = [("","---")]+[(x, x) for x in os.listdir(
                 "ubumlaas/datasets/"+current_user.username)]
 
     submit = SubmitField("Create")
@@ -35,6 +36,21 @@ class ExperimentForm(FlaskForm):
 
 class DatasetForm(FlaskForm):
     dataset = FileField()
-    target_column = SelectField("Target column", validators=[DataRequired()], choices=[])
+    def to_dataframe(self, filename, upload_folder):
+        if filename.split(".")[-1] == "csv":
+            file_df = pd.read_csv(upload_folder + filename)
+        elif filename.split(".")[-1] == "xls":
+            file_df = pd.read_excel(upload_folder + filename)
+        else:
+            flash("File format not allowed")
+        return file_df
+
+class DatasetParametersForm(FlaskForm):
     separator = SelectField("Column separator", validators=[DataRequired()], choices=[("column", ":"), ("semicolon", ";"), ("tab", "tab"), ("comma", ",")])
     train_partition = IntegerRangeField("Train/Test partition", validators=[NumberRange(0,100)])
+
+class DatasetTargetForm(FlaskForm):
+    target_column = SelectField("Target column", validators=[DataRequired()], choices=[])
+
+    def add_target_candidates(self, columns):
+        self.target_column.choices = [(x,x) for x in columns]
