957683f09d70d52fe9995d75c4e965a84efa3065
JoseluCross <joselucross@jkanetwork.com>
2019-09-20 17:22:47 +0200

    Add weka, TODO complete it

--- ubumlaas/experiments/algorithm.py
+++ ubumlaas/experiments/algorithm.py
@@ -25,43 +25,22 @@ from weka.classifiers import Evaluation
 def task_skeleton(experiment, current_user):
     # Task need app environment
     create_app('subprocess')
-
-    apps_functions={"sklearn":execute_sklearn, "weka": execute_weka}
-    
-    type_app=experiment["alg"]["alg_name"].split(".",1)[0]
-
-    result,state = apps_functions[type_app](experiment,current_user)
-
-    from ubumlaas.models import Experiment
-
-    exp = Experiment.query.filter_by(id=experiment['id']).first()
-
-    exp.result = result
-    exp.state = state
-    exp.endtime = time()
-    v.db.session.commit()
-
-    send_email(current_user["username"], current_user["email"], experiment["id"], str(exp.result))
-
-
-def execute_sklearn(experiment, current_user):
+    apps_functions = {"sklearn": execute_sklearn, "weka": execute_weka}
+    type_app = experiment["alg"]["alg_name"].split(".", 1)[0]
     try:
-        model = eval(experiment["alg"]["alg_name"]+"()")
         exp_config = json.loads(experiment["exp_config"])
-        data = pd.read_csv("ubumlaas/datasets/"+current_user["username"]
-        +"/"+experiment['data'])
+        data = pd.read_csv("ubumlaas/datasets/"+current_user["username"] +
+                           "/"+experiment['data'])
         X = data.loc[:, data.columns != exp_config["target"]]
-        print(X.head())
         y = data[exp_config["target"]]
-        print(y.head())
         X_train, X_test, y_train, y_test = \
             sklearn.model_selection. \
             train_test_split(X, y,
                              train_size=exp_config["split"]/100,
                              test_size=1-exp_config["split"]/100)
-        model.fit(X_train, y_train)
-        print(exp_config["split"])
 
+        model = apps_functions[type_app](experiment, X_train, y_train)
+  
         y_pred = model.predict(X_test)
         score_text = ""
         score = 0
@@ -71,36 +50,50 @@ def execute_sklearn(experiment, current_user):
         elif experiment["alg"]["alg_typ"] == "Classification":
             score_text = "Confussion Matrix"
             score = sklearn.metrics.confusion_matrix(y_test, y_pred)
-        result = score_text+": "+str(score)
-        state = 1
+
     except Exception as ex:
         result = str(ex)
         raise
         state = 2
 
+    from ubumlaas.models import Experiment
+
+    exp = Experiment.query.filter_by(id=experiment['id']).first()
+    exp.result = result
+    exp.state = state
+    exp.endtime = time()
+    v.db.session.commit()
+
+    send_email(current_user["username"], current_user["email"], experiment["id"], str(exp.result))
+
+
+def execute_sklearn(experiment, X_train, y_train):
+    model = eval(experiment["alg"]["alg_name"]+"()")
+    model.fit(X_train, y_train)
+
+    return model
+
+    
+    result = score_text+": "+str(score)
+    state = 1
+
+
     return result, state
 
-def execute_weka(experiment, current_user):
-    try:
-        jvm.start()
-        data_dir = "ubumlaas/datasets/"+current_user["username"]+"/"+experiment["data"]
-        data = converters.load_any_file(data_dir)
-        #Last column of data is target
-        data.class_is_last()
+def execute_weka(experiment, X_train, y_train):
+    jvm.start()
+    data_dir = "ubumlaas/datasets/"+current_user["username"]+"/"+experiment["data"]
+    data = converters.load_any_file(data_dir)
+    #Last column of data is target
+    data.class_is_last()
 
-        classifier = Classifier(classname=experiment["alg"]["alg_name"])
-        
+    classifier = Classifier(classname=experiment["alg"]["alg_name"])
+    
+    evaluation = Evaluation(data)
+    evaluation.evaluate_train_test_split(classifier, data, 70.0,Random(time()))
+    result = evaluation.summary()
+    state=1        
 
-        evaluation = Evaluation(data)
-        evaluation.evaluate_train_test_split(classifier, data, 70.0,Random(time()))
-        result = evaluation.summary()
-        state=1
-        
-    except Exception as ex:
-        result = str(ex)
-        #raise
-        state = 2
-    finally:
-        jvm.stop()
+    jvm.stop()
 
-    return result,state
+    return result, state
