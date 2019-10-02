1839b7234e9206c03fa7aceaae859d44edfcf1e8
Yi <yjx0003@alu.ubu.es>
2019-10-02 09:39:15 +0200

    #56 Download train model

--- ubumlaas/experiments/views.py
+++ ubumlaas/experiments/views.py
@@ -202,7 +202,7 @@ def form_generator():
     return jsonify({"alg_config": alg.config})
 
 @login_required
-@experiments.route("/experiment/<int:id>.model")
+@experiments.route("/experiment/<int:id>/download_model")
 def download_model(id):
     """Download model file using the experiment id
     
@@ -212,10 +212,17 @@ def download_model(id):
     Returns:
         file -- model file
     """
-    filename = "{}.model".format(id)
-    upload_path = safe_join("models/"+current_user.username+"/"+filename, filename)
 
+    exp = load_experiment(id)
+
+    if not exp or exp.idu != current_user.id:
+        abort(404)
+ 
+    path = safe_join("models/"+current_user.username+"/","{}.model".format(id))
+
+    download_filename = "UBUMLaaS_{}_{}.model".format(id, get_algorithm_by_name(exp.alg_name).lib)
+    
     try:
-        return send_file(upload_path, filename)
+        return send_file(path, attachment_filename=download_filename, as_attachment=True)
     except FileNotFoundError:
         abort(404)
