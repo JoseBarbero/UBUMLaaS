4c922265ed7e0b4ce02bfd429a27a5b38931988c
Daniel Puente <dpr1005@alu.ubu.es>
2022-02-04 13:35:10 +0100

    Updated SSL execution structure #4

--- ubumlaas/experiments/execute_algorithm/_is_ssl.py
+++ ubumlaas/experiments/execute_algorithm/_is_ssl.py
@@ -7,7 +7,10 @@ import sklearn.ensemble
 import sklearn.model_selection
 import sklearn.utils
 import variables as v
+import pickle
+from imblearn.pipeline import make_pipeline
 from ubumlaas.experiments.execute_algorithm import Abstract_execute
+import lib.is_ssl.semisupervised
 
 class Execute_ssl(Abstract_execute):
     
@@ -22,38 +25,87 @@ class Execute_ssl(Abstract_execute):
 
     @staticmethod
     def __create_model(alg_name, alg_config):
+        """Create a model recursive
+
+        Arguments:
+            alg_name {string} -- model name
+            alg_config {dict} -- parameters
         """
-        
-        """
-        pass
+        for ac in alg_config:
+            if type(alg_config[ac]) == dict:
+                alg_config[ac] = Execute_ssl.__create_model(
+                    alg_config[ac]["alg_name"],
+                    alg_config[ac]["parameters"])
+
+        model = eval("lib."+alg_name+"(**alg_config)")
+        return model
 
     def create_model(self):
+        """Create the model with experiment
+
+        Returns:
+            [object] -- model with the experiment configuration or pipeline if has filter
         """
-        
-        """
-        pass
+        model = Execute_ssl.__create_model(
+            self.algorithm_name, self.algorithm_configuration)
+
+        if self.has_filter():
+            filter = eval(self.filter_name+"(**self.filter_config)")
+            return make_pipeline(filter, model)
+        return model
 
     def serialize(self, model, path):
+        """Serialize the model in specific path
+
+        Arguments:
+            model {object} -- the model to serialize
+            path {str} -- path to save the model serialized
         """
-        
-        """
-        pass
+        v.app.logger.info("Model saved - %d - %s",
+                          self.id, self.algorithm_name)
+        pickle.dump(model, open(path, 'wb'))
 
     def deserialize(self, path):
+        """Deserialize the model in the specific path
+
+        Arguments:
+            path {str} -- path with the model file saved
+
+        Returns:
+            [object] -- model deserialized
         """
-        
-        """
-        pass
+        v.app.logger.info("Model readed - %d - %s",
+                          self.id, self.algorithm_name)
+        return pickle.load(open(path, 'rb'))
 
     def train(self, model, X, y):
+        """Train the model with attributes columns (X) and targets columns (Y)
+
+        Arguments:
+            model {object} -- model to train
+            X {DataFrame} -- attributes columns
+            y {FataFrame} -- targets columns
         """
+        v.app.logger.info("Training model - %d - %s",
+                          self.id, self.algorithm_name)
         
-        """
-        pass
+        model.fit(X, y.values.ravel())
+        v.app.logger.info("Model trained - %d - %s",
+                          self.id, self.algorithm_name)
 
     def predict(self, model, X):
+        """Predict with X columns values using the model
+
+        Arguments:
+            model {object} -- model to use for predictions
+            X {DataFrame} -- dataframe with X columns values
+
+        Returns:
+            list, list -- predictions values, distribution class if is classified or None otherwise
         """
-        
-        """
-        pass
+        predictions = model.predict(X)
+        y_score = None
+        if self.is_semi_supervised_calssification():
+            y_score = model.predict_proba(X)
+        return predictions, y_score
     
