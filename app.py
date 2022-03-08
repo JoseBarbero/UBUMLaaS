a4834cecc3f6c6206be57074a6d7ab3f086bd22c
Daniel Puente <dpr1005@alu.ubu.es>
2022-03-08 13:52:19 +0100

    Updated app.py so it canwork both intravis and user usage #44

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from ubumlaas import create_app
 import signal
 import threading
+import sys
 
 """
 Main, run application.
 """
 if __name__ == "__main__":
-    create_app("main_app").run(host='0.0.0.0', port=8081)
+    try:
+        host = sys.argv[1] 
+    except IndexError:
+        host='localhost'
+    try:
+        port = sys.argv[2]
+    except IndexError:
+        port = 5000
+
+    create_app("main_app").run(host=host, port=port)
